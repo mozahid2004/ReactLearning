e62106dcd794bccc518a50aa5a56258b3de6c1a7
Mozahid Izhar <mozahidizhar54@gmail.com>
2025-02-08 12:32:41 +0530

    Initial commit with subfolders

---
+++ 02_Hooks/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(10)
+
+  const addValue = () => {
+    counter < 20 ? setCounter(counter + 1) : ""
+  }
+  const removeValue = () => {
+    counter > 0 ?  setCounter(counter - 1) : 0
+  }
+
+  return (
+    <>
+      <h1>Mozahid Khan</h1>
+      <h2>counter : {counter}</h2>
+
+    
+      <button onClick={addValue}>Add Value : {counter}</button>
+      <button onClick={removeValue}>Remove Value : {counter}</button>
+
+      <h2>Footer : {counter}</h2>
+    </>
+  )
+}
+
+export default App
